33961423e19e66be879aaedd87d0136aa78cc6f0
liuyi27 <liuyi27@58.com>
2020-03-25 18:15:09 +0800

    first commit

---
+++ src/main/java/com/fire/registrationcenter/RegistrationCenterApplication.java
@@ -0,0 +1,15 @@
+package com.fire.registrationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistrationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationCenterApplication.class, args);
+    }
+
+}
